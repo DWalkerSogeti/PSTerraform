663648d19ae35dc2e08fa07927e85668ee9a9900
DWalkerSogeti <devin.walker@sogeti.com>
2022-04-22 17:22:53 -0500

    Adding keys for automation

--- Getting-Started-Terraform/commands/m5_commands.sh
+++ Getting-Started-Terraform/commands/m5_commands.sh
@@ -11,8 +11,8 @@ terraform validate
 
 # In case you don't have them set anymore don't forget to run the export commands
 # For Linux and MacOS
-export TF_VAR_aws_access_key=YOUR_ACCESS_KEY
-export TF_VAR_aws_secret_key=YOUR_SECRET_KEY
+export TF_VAR_aws_access_key=AKIAQYSCJLPYMZLFFBNW
+export TF_VAR_aws_secret_key=1HoCcLuCmoOGA27Wyd2bNkaKjnoRUyT0QZgFfDie
 
 # For PowerShell
 $env:TF_VAR_aws_access_key="AKIAQYSCJLPYMZLFFBNW"
