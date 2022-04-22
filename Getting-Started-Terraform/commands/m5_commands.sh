3ec0418bc0b05b1645836bf4623eaaa725798579
DWalkerSogeti <devin.walker@sogeti.com>
2022-04-22 16:49:05 -0500

    Updating output for public dns

--- Getting-Started-Terraform/commands/m5_commands.sh
+++ Getting-Started-Terraform/commands/m5_commands.sh
@@ -15,8 +15,8 @@ export TF_VAR_aws_access_key=YOUR_ACCESS_KEY
 export TF_VAR_aws_secret_key=YOUR_SECRET_KEY
 
 # For PowerShell
-$env:TF_VAR_aws_access_key="YOUR_ACCESS_KEY"
-$env:TF_VAR_aws_secret_key="YOUR_SECRET_KEY"
+$env:TF_VAR_aws_access_key="AKIAQYSCJLPYMZLFFBNW"
+$env:TF_VAR_aws_secret_key="1HoCcLuCmoOGA27Wyd2bNkaKjnoRUyT0QZgFfDie"
 
 terraform plan -out m5.tfplan
 terraform apply m5.tfplan
