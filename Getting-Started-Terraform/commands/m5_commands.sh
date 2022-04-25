49bbeb83823753c92f2a0b7077cfba2b3d258542
DWalkerSogeti <102998825+DWalkerSogeti@users.noreply.github.com>
2022-04-25 09:54:17 -0500

    Update m5_commands.sh

--- Getting-Started-Terraform/commands/m5_commands.sh
+++ Getting-Started-Terraform/commands/m5_commands.sh
@@ -11,12 +11,12 @@ terraform validate
 
 # In case you don't have them set anymore don't forget to run the export commands
 # For Linux and MacOS
-export TF_VAR_aws_access_key=AKIAQYSCJLPYMZLFFBNW
-export TF_VAR_aws_secret_key=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
+export TF_VAR_aws_access_key=
+export TF_VAR_aws_secret_key=
 
 # For PowerShell
-$env:TF_VAR_aws_access_key="AKIAQYSCJLPYMZLFFBNW"
-$env:TF_VAR_aws_secret_key="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"
+$env:TF_VAR_aws_access_key=""
+$env:TF_VAR_aws_secret_key=""
 
 terraform plan -out m5.tfplan
 terraform apply m5.tfplan
