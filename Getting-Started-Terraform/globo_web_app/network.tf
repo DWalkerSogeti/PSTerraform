dfa7799fc6391e2dfefbf585eaa318b95dac375d
DWalkerSogeti <devin.walker@sogeti.com>
2022-04-27 16:04:27 -0500

    Module 7 Cidrsubnet function

--- Getting-Started-Terraform/globo_web_app/network.tf
+++ Getting-Started-Terraform/globo_web_app/network.tf
@@ -30,7 +30,7 @@ resource "aws_internet_gateway" "igw" {
 
 resource "aws_subnet" "subnets" {
   count = var.vpc_subnet_count
-  cidr_block              = var.vpc_subnets_cidr_blocks[count.index]
+  cidr_block              = cidrsubnet(var.vpc_cidr_block, 8, count.index)
   vpc_id                  = aws_vpc.vpc.id
   map_public_ip_on_launch = var.map_public_ip_on_launch
   availability_zone       = data.aws_availability_zones.available.names[count.index]
